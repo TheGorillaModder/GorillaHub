2eeb1340f9f36859aab22bf3dd6e16406e5b2e4c
TheGorillaModder <thegorillamodder@gmail.com>
2026-01-12 20:55:10 -0500

    Entire website
    Added all files from main local repository directly from my laptop.

---
+++ Sources/.2Pages/gameSections.js
@@ -0,0 +1,115 @@
+const gameSections = [
+    {
+        title: "Uncatigorized",
+        games: [
+            //{ name: "Games Websites", link: "Sources/.2Pages/GamesPages.html" },
+            //{ name: "Minecraft 1.3", link: "Sources/Uncatigorized/Minecraft1.3.html" },
+            //{ name: "Minecraft 1.5", link: "Sources/Uncatigorized/Minecraft1.5.html" },
+            { name: "Minecraft 1.8", link: "Sources/Uncatigorized/Minecraft1.8.html" },
+            //{ name: "Startup Simulator", link: "Sources/Uncatigorized/StartupSimulator.html" },
+            //{ name: "You VS 100 Skibidi Toilets", link: "Sources/Uncatigorized/UVS100Skibidis.html" },
+        ]
+    },
+    {
+        title: "Driving",
+        games: [
+            { name: "Car Crash", link: "Sources/Driving/CarCrash.html" },
+            //{ name: "Car Crash 1", link: "Sources/Driving/CarCrash1.html" },
+            //{ name: "Car Crash 2", link: "Sources/Driving/CarCrash2.html" },
+            { name: "Car Crash 3", link: "Sources/Driving/CarCrash3.html" },
+            //{ name: "Car Driving Drift", link: "Sources/Driving/CarDrivingDrift.html" },
+            { name: "Car Physics", link: "Sources/Driving/CarPhysics.html" },
+            //{ name: "Drift Hunters RU", link: "Sources/Driving/DriftHuntersRU.html" },
+            //{ name: "Racing Arena", link: "Sources/Driving/RacingArena.html" },
+            //{ name: "Sky Race 3D", link: "Sources/Driving/SkyRace3D.html" },
+            { name: "Slow Roads", link: "Sources/Driving/SlowRoads.html" },
+            { name: "Supercar Battle Royale", link: "Sources/Driving/SupercarBattleRoyale.html" },
+            { name: "Traffic Run", link: "Sources/Driving/TrafficRun.html" },
+        ]
+    },
+    {
+        title: "Shooters",
+        games: [
+            //{ name: "CS 1.6", link: "Sources/Shooters/CS1.6.html" },
+            //{ name: "Gun Mayhem", link: "Sources/Shooters/GunMayhem.html" },
+            //{ name: "Gun Mayhem 2", link: "Sources/Shooters/GunMayhem2.html" },
+            { name: "Ragdoll Archers", link: "Sources/Shooters/RagdollArchers.html" },
+        ]
+    },
+    {
+        title: "Platformers",
+        games: [
+            { name: "Vex 3", link: "Sources/Platformers/Vex3.html" },
+            //{ name: "Vex 4", link: "Sources/Platformers/Vex4.html" },
+            //{ name: "Vex 5", link: "Sources/Platformers/Vex5.html" },
+            //{ name: "Vex 6", link: "Sources/Platformers/Vex6.html" },
+            //{ name: "Vex 8", link: "Sources/Platformers/Vex8.html" },
+        ]
+    },
+    {
+        title: ".io Games",
+        games: [
+            //{ name: "CleanUp.io", link: "Sources/io/CleanUp.html" },
+            { name: "Doomz.io", link: "Sources/io/Doomz.html" },
+            //{ name: "Paper.io 2", link: "Sources/io/Paper2.html" },
+            { name: "Papers.io Mania", link: "Sources/io/PapersMania.html" },
+            //{ name: "Penguin.io", link: "Sources/io/Penguin.html" },
+            //{ name: "Waterpool.io", link: "Sources/io/Waterpool.html" },
+        ]
+    },
+    {
+        title: "Puzzles",
+        games: [
+            //{ name: "Block Blast", link: "Sources/Puzzles/BlockBlast.html" },
+            //{ name: "Bloxorz", link: "Sources/Puzzles/Bloxorz.html" },
+        ]
+    },
+    {
+        title: "Endless Runners",
+        games: [
+            { name: "Run 3", link: "Sources/EndlessRunners/Run3.html" },
+        ]
+    },
+    {
+        title: "Fun",
+        games: [
+            //{ name: "Poly Track", link: "https://gooooooooooooooooooooooooooooooooooooooooooogle-opensocial.googleusercontent.com/gadgets/ifr?url=https://cdn.jsdelivr.net/gh/wergboy/NLP@3cd7c854d047f364125194f75adeaa75aecf7985/pt/pt.xml&container=ig" },
+            //{ name: "Hobo", link: "Sources/Fun/Hobo.html" },
+            //{ name: "Hobo 3 Wanted", link: "Sources/Fun/Hobo3Wanted.html" },
+            //{ name: "Hobo 6", link: "Sources/Fun/Hobo6.html" },
+            //{ name: "Hobo Prison Brawl", link: "Sources/Fun/HoboPrisonBrawl.html" },
+            //{ name: "Learn To Fly Idle", link: "Sources/Fun/LearnToFlyIdle.html" },
+            //{ name: "Shopping Cart Hero", link: "Sources/Fun/ShoppingCartHero.html" },
+        ]
+    },
+    {
+        title: "Scratch Games",
+        games: [
+            { name: "Not Quite Ball Physics", link: "https://scratch.mit.edu/projects/16586232/embed" },
+            { name: "Getting Over It", link: "https://scratch.mit.edu/projects/389464290/embed" },
+            { name: "Paper Minecraft", link: "https://scratch.mit.edu/projects/10128407/embed" },
+        ]
+    },
+    {
+        title: "Simple",
+        games: [
+            { name: "Snake-ish", link: "Sources/Simple/Snake/index.html" },
+            { name: "Breakout", link: "Sources/Simple/Breakout.html" },
+        ]
+    },
+    {
+        title: "Unblocked?",
+        games: [
+            { name: "Nation States (Create A Nation)", link: "https://www.nationstates.net" },
+            { name: "Sim CB", link: "https://benoitessiambre.com/macro.html" },
+            { name: "Money Simulator", link: "https://simulator.money" },
+        ]
+    },
+    {
+        title: "Blocked?",
+        games: [
+            { name: "Krunker", link: "https://krunker.io" },
+            { name: "Kour.io", link: "https://kour.io" },
+        ]
+    }
+];
